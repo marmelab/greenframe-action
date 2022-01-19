4acf4bd574f5343dc62587c31782d2f33c5a9473
Clément Le Biez <clement@marmelab.com>
2022-01-19 15:29:53 +0100

    test action

--- src/examples/exampleOfScenario.js
+++
@@ -1,6 +0,0 @@
-async (page) => {
-    await page.goto('', {
-        waitUntil: 'networkidle',
-    });
-    await page.scrollToElement('footer');
-};
--- src/examples/moviedb.js
+++
@@ -1,9 +0,0 @@
-async (page) => {
-    await page.goto('', {
-        waitUntil: 'networkidle',
-    });
-    await page.waitForTimeout(5000);
-    await page.scrollToElement('text="Luca"');
-    await page.click('text="Luca"');
-    await page.waitForTimeout(3000);
-};
